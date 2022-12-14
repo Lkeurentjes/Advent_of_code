e1c9bcca29d00860ac9393ac5d754ad5c5068cc5
Lisa Keurentjes <43202623+Lkeurentjes@users.noreply.github.com>
2022-12-14 12:39:00 +0100

    1207 & 1214

---
+++ 1207storage.py
@@ -0,0 +1,86 @@
+#
+# drivesdict = {"/":[0]}
+# drivessize = 0
+#
+#
+# def recursivesize(sizes):
+#     sumsize = sizes[0]
+#     if sumsize <= 100000:
+#         return sumsize
+#     else:
+#         for i in range(1,len(sizes)):
+#             sumsize += recursivesize(drivesdict[sizes[i]])
+#         if sumsize <= 100000:
+#             return sumsize
+#         else:
+#             return 0
+#
+#
+#
+# with open("1207storage.txt") as f:
+#     lines = f.read().splitlines()
+#     print(lines)
+#     dirnow = None
+#     for line in lines:
+#         line = line.split(" ")
+#         # print(line)
+#         if len(line) == 3:
+#             dirnow = line[2]
+#             continue
+#         if line[0] == "$":
+#             continue
+#         try:
+#             size = int(line[0])
+#         except:
+#             drivesdict[dirnow].append(line[1])
+#             drivesdict[line[1]] = [0]
+#             continue
+#         drivesdict[dirnow][0] += size
+#
+#     print(drivesdict)
+#     for sizes in drivesdict.values():
+#         print(sizes)
+#         print(recursivesize(sizes))
+#         drivessize += recursivesize(sizes)
+#         print(drivessize)
+#
+
+from collections import defaultdict
+
+with open('1207storage.txt') as f:
+  executions = f.read().strip().split('$ ')
+
+tree = {}
+pwd = []
+
+for execution in executions[1:]:
+  command, *output = execution.splitlines()
+  program, *args = command.split(' ')
+  if program == 'ls':
+    for line in output:
+      size, name = line.split(' ')
+      if size != 'dir':
+        tree[tuple(pwd) + (name,)] = int(size)
+  else: # program == 'cd'
+    if args[0] == '..':
+      pwd.pop()
+    else:
+      pwd.append(args[0])
+
+sizes = defaultdict(int)
+for path, size in tree.items():
+  pwd = []
+  for dir in path:
+    sizes[tuple(pwd)] += size
+    pwd.append(dir)
+
+print(sum([size for size in sizes.values() if size <= 100000]))
+
+total = 70000000
+needed = 30000000
+taken = sizes[()]
+
+for size in sorted(sizes.values()):
+  if total - (taken - size) >= needed:
+    print(size)
+    break
---
+++ 1214sand.py
@@ -0,0 +1,124 @@
+import numpy as np
+
+class Grid:
+    def __init__(self, lines):
+        print(lines)
+        self.sandpaths = []
+        self.maxx = 0
+        self.minx = 1000
+        self.maxy = 0
+        self.miny = 1000
+        for line in lines:
+            sandpath = []
+            line = line.split(" -> ")
+            for l in line:
+                l = list(map(int,l.split(",")))
+                if l[0]<self.minx:
+                    self.minx = l[0]
+                if l[0]>self.maxx:
+                    self.maxx = l[0]
+                if l[1]>self.maxy:
+                    self.maxy = l[1]
+                sandpath.append(l)
+            self.sandpaths.append(sandpath)
+        # print(self.sandpaths)
+        # print(self.minx, self.maxx, self.maxy)
+        #part 2 stuff
+        self.widder = (self.maxy+2)*2
+        self.maxy+=2
+
+        self.width = self.maxx-self.minx+1+self.widder
+        self.grid = np.zeros((self.maxy+1, self.width))
+
+        self.minx -= (self.widder // 2)
+        # self.grid[0][500-self.minx] = 77
+        self.makesandpaths()
+
+
+
+
+    def makesandpaths(self):
+        for path in self.sandpaths:
+
+            for i in range(len(path)-1):
+
+                stepH = path[i+1][0] -path[i][0]
+                if stepH < 0:
+
+                    for j in range(0,stepH-1,-1):
+                        self.grid[path[i][1]][path[i][0] - self.minx +j] = 88
+
+                if stepH > 0:
+
+                    for j in range(0,stepH+1):
+                        self.grid[path[i][1] ][path[i][0] - self.minx +j] = 88
+
+
+                stepV =path[i + 1][1] - path[i][1]
+                if stepV < 0:
+
+                    for j in range(0,stepV-1,-1):
+                        self.grid[path[i][1] +j][path[i][0] - self.minx] = 88
+                if stepV > 0:
+
+                    for j in range(0,stepV+1):
+                        self.grid[path[i][1] +j][path[i][0] - self.minx] = 88
+
+        for i in range(self.width):
+            self.grid[self.maxy][i] = 88
+
+        np.savetxt("1214matrix1.txt", self.grid, fmt='% 4d')
+
+
+    def sandflow(self):
+        counter = 0
+        stop=True
+        while stop:
+            coord = self.horizontalfinder(500-self.minx,0)
+            # print(coord)
+            if coord == (10000,10000):
+                stop = False
+            if coord == (500 - self.minx,0):
+                counter += 1
+                self.grid[coord[1]][coord[0]] = 1
+                stop = False
+            else:
+                counter += 1
+                self.grid[coord[1]][coord[0]] = 1
+                np.savetxt("1214matrix.txt", self.grid, fmt='% 4d')
+                # print(self.grid)
+
+
+        # self.grid.tofile("1214matrix.txt")
+        np.savetxt("1214matrix.txt",self.grid, fmt='% 4d')
+        return counter
+
+    def diagonalfinder(self,X,Y):
+        if self.grid[Y][X-1] == 0:
+            return self.horizontalfinder(X-1, Y)
+        if self.grid[Y][X+1] == 0:
+            return self.horizontalfinder(X+1, Y)
+        else:
+            return (X,Y-1)
+
+    def horizontalfinder(self,X,Y):
+        if X<0 or Y<0 or X>self.width or Y>self.maxy:
+            return (10000, 10000)
+        print(X,Y, self.maxy)
+        for i in range(Y,self.maxy+1,1):
+            print(X,i)
+            if self.grid[i][X] != 0:
+                return self.diagonalfinder(X,i)
+        print("\n\n\n\n\nI SHOULD NOT BE HEEEEERE\n\n\n\n\n")
+        return (10000,10000)
+
+
+
+
+
+
+
+with open("1214sand.txt") as f:
+    lines = f.read().splitlines()
+    landscape = Grid(lines)
+    print(landscape.sandflow())
