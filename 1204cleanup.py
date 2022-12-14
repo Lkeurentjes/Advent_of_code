365a9d7b42fb5377a823f86f43e8bf85e8d9e5ae
Lisa Keurentjes <43202623+Lkeurentjes@users.noreply.github.com>
2022-12-14 14:44:08 +0100

    1205&1206

--- 1204cleanup.py
+++ 1204cleanup.py
@@ -1,4 +1,3 @@
-import re
 with open("1204cleanup.txt") as f:
     lines = f.read().splitlines()
     print(lines)
@@ -7,8 +6,6 @@ counter = 0
 for l in lines:
     l = list(map(int,l.replace("-",",").split(",")))
     print(l)
-    # print(l[0], l[2],l[1], l[3])
-    # print(l[0] >= l[2] , l[1] <= l[3], l[2] >= l[0] , l[3] <= l[1])
     # if (l[0] >= l[2] and l[1] <= l[3]) or (l[2] >= l[0] and l[3] <= l[1]):
     if ((l[0] in range(l[2],l[3]+1)) or (l[1] in range(l[2],l[3]+1)) or (l[2] in range(l[0],l[1]+1)) or (l[3] in range(l[0],l[1]+1)) ):
         counter += 1
