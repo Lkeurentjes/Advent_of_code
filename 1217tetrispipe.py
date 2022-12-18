cb05b44050577a371d1e869d8755b1b56172cedc
Lisa Keurentjes <43202623+Lkeurentjes@users.noreply.github.com>
2022-12-18 11:06:02 +0100

    1218completed

---
+++ 1217tetrispipe.py
@@ -0,0 +1,231 @@
+import numpy as np
+
+class Grid:
+    def __init__(self, jetstream):
+        self.jetstream = jetstream
+        self.jetindex = 0
+
+        self.width = 7
+        self.emptyheight = 3
+        self.height = 4
+        self.pipe = np.vstack(([8,8,8,8,8,8,8], np.zeros((self.emptyheight,self.width))))
+
+        self.shape1 = [[1,1,1,1]]
+        self.shape2 = [[0, 1, 0],
+                       [1, 1, 1],
+                       [0, 1, 0]]
+        self.shape3 = [[1, 1, 1],
+                       [0, 0, 1],
+                       [0, 0, 1]]
+        self.shape4 = [[1],
+                       [1],
+                       [1],
+                       [1]]
+        self.shape5 = [[1, 1],
+                       [1, 1]]
+
+        self.checkleft = {1:[(0,0)],
+                          2:[(1,0),(0,0),(2,0)],
+                          3:[(0,0),(1,0),(2,0)],
+                          4:[(0,0),(1,0),(2,0),(3,0)],
+                          5:[(0,0),(1,0)]}
+        self.checkright = {1:[(0,3)],
+                           2:[(1,2),(0,0),(2,0)],
+                           3:[(0,2),(1,0),(2,0)],
+                           4:[(0,0),(1,0),(2,0),(3,0)],
+                           5:[(0,1),(1,1)]}
+        self.checkbottem = {1:[(0,0),(0,1),(0,2),(0,3)],
+                            2:[(0,0),(1,0),(1,2)],
+                            3:[(0,0),(0,1),(0,2)],
+                            4:[(0,0)],
+                            5:[(0,0),(0,1)]}
+
+        self.shapeslist = [self.shape1,self.shape2,self.shape3,self.shape4,self.shape5]
+        # for shape in self.shapeslist:
+        #     print(shape)
+
+    def printpipebackwards(self):
+        for row in self.pipe[::-1]:
+            print(row)
+
+    def checkRIGHT(self,shape,shapenumber,addedpipe):
+        for check in self.checkright[shapenumber]:
+            if shape[check[0]][check[1]][1]==6:
+                return False
+            if addedpipe[shape[check[0]][check[1]][0]][shape[check[0]][check[1]][1]+1] != 0:
+                return False
+        return True
+
+    def checkLEFT(self,shape,shapenumber,addedpipe):
+        for check in self.checkleft[shapenumber]:
+            if shape[check[0]][check[1]][1]==0:
+                return False
+            if addedpipe[shape[check[0]][check[1]][0]][shape[check[0]][check[1]][1]-1] != 0:
+                return False
+        return True
+
+    def push(self,shape,shapenumber,addedpipe,stopped = False):
+        if self.jetindex == len(self.jetstream)-1:
+            self.jetindex = 0
+
+        # print(self.jetstream[self.jetindex])
+        if self.jetstream[self.jetindex] == ">":
+            self.jetindex += 1
+            if self.checkRIGHT(shape,shapenumber,addedpipe):
+                for i in range(len(shape)):
+                    for j in range(len(shape[i])):
+                        shape[i][j][1] += 1
+                if stopped:
+                    if self.checkstop(shape, shapenumber, addedpipe):
+                        self.stop(shape, shapenumber)
+                    else:
+                        self.down(shape, shapenumber, addedpipe)
+                else:
+                    self.down(shape, shapenumber, addedpipe)
+
+            else:
+                if stopped:
+                    self.stop(shape, shapenumber)
+                else:
+                    self.down(shape, shapenumber, addedpipe)
+
+
+        elif self.jetstream[self.jetindex] == "<":
+            self.jetindex += 1
+            if self.checkLEFT(shape,shapenumber,addedpipe):
+                for i in range(len(shape)):
+                    for j in range(len(shape[i])):
+                        shape[i][j][1] -= 1
+
+                if stopped:
+                    if self.checkstop(shape, shapenumber, addedpipe):
+                        self.stop(shape, shapenumber)
+                    else:
+                        self.down(shape, shapenumber, addedpipe)
+                else:
+                    self.down(shape, shapenumber, addedpipe)
+
+            else:
+                if stopped:
+                    self.stop(shape, shapenumber)
+                else:
+                    self.down(shape, shapenumber, addedpipe)
+
+
+
+    def checkstop(self,shape,shapenumber,addedpipe):
+        # print(shape , "CHECKED")
+        for check in self.checkbottem[shapenumber]:
+            if addedpipe[shape[check[0]][check[1]][0] - 1][shape[check[0]][check[1]][1]] != 0:
+                return True
+        return False
+
+
+    def down(self,shape,shapenumber,addedpipe):
+        for i in range(len(shape)):
+            for j in range(len(shape[i])):
+                shape[i][j][0] -= 1
+
+        if self.checkstop(shape, shapenumber, addedpipe):
+            # self.stop(shape,shapenumber)
+            self.push(shape, shapenumber, addedpipe,True)
+        else:
+            self.push(shape, shapenumber, addedpipe)
+
+
+
+
+
+    def stop(self,shape,shapenumber):
+        # print(shape)
+        # print("\n")
+        for i in range(len(shape)):
+            for j in range(len(shape[i])):
+                coord = shape[i][j]
+                self.pipe[coord[0]][coord[1]] = shapenumber
+
+        emptyrows = 0
+        for row in self.pipe[::-1]:
+            sum = np.sum(row)
+            if sum != 0:
+                break
+            else:
+                emptyrows +=1
+
+        for i in range(3-emptyrows):
+            self.pipe = np.vstack((self.pipe, [0, 0, 0, 0, 0, 0, 0]))
+        self.height += (3-emptyrows)
+        # print(self.printpipebackwards())
+
+
+    def makeshape_coord(self, shape):
+        shapecoord = []
+        for i in range(len(shape)):
+            row = []
+            for j in range(len(shape[i])):
+                if shape[i][j] != 0:
+                    coord = [self.height + i, 2 + j]
+                    row.append(coord)
+            shapecoord.append(row)
+        # print(shapecoord)
+        return shapecoord
+
+
+    def fallingdown(self, rocks):
+        rocksfive = rocks//5 +1
+        rock = 0
+        for i in range(rocksfive):
+
+            if rock<rocks:
+                rock+=1
+                addedpipe = np.vstack((self.pipe,[0,0,0,0,0,0,0]))
+                shape = self.makeshape_coord(self.shape1)
+                self.push(shape,1,addedpipe)
+
+
+            if rock<rocks:
+                rock+=1
+                addedpipe = np.vstack((self.pipe, [0, 0, 0, 0, 0, 0, 0]))
+                addedpipe = np.vstack((addedpipe, [0, 0, 0, 0, 0, 0, 0]))
+                addedpipe = np.vstack((addedpipe, [0, 0, 0, 0, 0, 0, 0]))
+                shape = self.makeshape_coord(self.shape2)
+                self.push(shape, 2, addedpipe)
+
+
+            if rock<rocks:
+                rock+=1
+                addedpipe = np.vstack((self.pipe, [0, 0, 0, 0, 0, 0, 0]))
+                addedpipe = np.vstack((addedpipe, [0, 0, 0, 0, 0, 0, 0]))
+                addedpipe = np.vstack((addedpipe, [0, 0, 0, 0, 0, 0, 0]))
+                shape = self.makeshape_coord(self.shape3)
+                self.push(shape, 3, addedpipe)
+
+
+            if rock<rocks:
+                rock+=1
+                addedpipe = np.vstack((self.pipe, [0, 0, 0, 0, 0, 0, 0]))
+                addedpipe = np.vstack((addedpipe, [0, 0, 0, 0, 0, 0, 0]))
+                addedpipe = np.vstack((addedpipe, [0, 0, 0, 0, 0, 0, 0]))
+                addedpipe = np.vstack((addedpipe, [0, 0, 0, 0, 0, 0, 0]))
+                shape = self.makeshape_coord(self.shape4)
+                self.push(shape, 4, addedpipe)
+
+
+            if rock<rocks:
+                rock+=1
+                addedpipe = np.vstack((self.pipe, [0, 0, 0, 0, 0, 0, 0]))
+                addedpipe = np.vstack((addedpipe, [0, 0, 0, 0, 0, 0, 0]))
+                shape = self.makeshape_coord(self.shape5)
+                self.push(shape, 5, addedpipe)
+
+
+
+
+with open("1217tetrispype.txt") as f:
+    line = f.read()
+    Pipe = Grid(line)
+    number_of_Rocks = 10
+    # number_of_Rocks = 2022
+    Pipe.fallingdown(number_of_Rocks)
+    Pipe.printpipebackwards()
+    print("height = ",Pipe.height-4)
---
+++ 1218lava.py
@@ -0,0 +1,111 @@
+
+import numpy as np
+from scipy.spatial import KDTree
+import copy
+import time
+import open3d as o3d
+
+with open("1218lava.txt") as f:
+    lines = f.read().splitlines()
+    number_of_cubes = len(lines)
+
+    max, min= 0,100
+
+    coordlist = []
+    for line in lines:
+        coord = []
+        line = line.split(",")
+        for i in range(len(line)):
+            coord.append(int(line[i]))
+            if int(line[i])> max:
+                max = int(line[i])
+            if int(line[i]) < min:
+                min = int(line[i])
+        coordlist.append(coord)
+    npcoord = np.array(coordlist)
+
+    # PART 1 ANSWER
+    kd_tree = KDTree(npcoord)
+    start_time = time.time()
+    count = kd_tree.count_neighbors(kd_tree, 1)
+    print("answer Part 1: ", number_of_cubes * 7 - count)
+    print("count neighbour took ", time.time() - start_time, " to run")
+
+    start_time = time.time()
+    indexes = kd_tree.query_ball_tree(kd_tree, r=1)
+    count2 = sum([len(i) for i in indexes])
+    print("answer Part 1: ", number_of_cubes * 7 - count2)
+    print("query ball took ", time.time() - start_time, " to run")
+
+    # # visualise point clouds
+    # pcd = o3d.geometry.PointCloud()
+    # pcd.points = o3d.utility.Vector3dVector(npcoord)
+    # o3d.visualization.draw_geometries([pcd])
+
+    print("\n")
+
+
+
+    # FOR PART 2 - FILL ALL THE CAVITIES
+    coordlistpart2 = copy.deepcopy(coordlist)
+    max +=1
+    min -=1
+
+    # all cells
+    emptygrid = []
+    for i in range(min,max):
+        for j in range(min,max):
+            for h in range(min,max):
+                if ([i, j, h]) not in coordlist:
+                    emptygrid.append([i, j, h])
+    emptylist = emptygrid
+    emptygrid = np.array(emptygrid)
+    kd_tree_empty = KDTree(emptygrid)
+    connection = kd_tree_empty.query_ball_tree(kd_tree_empty,1)
+
+    #find which are connected to the border
+    indexes_to_pop = [0]
+    for index in indexes_to_pop:
+        for add in connection[index]:
+            if add not in indexes_to_pop:
+                indexes_to_pop.append(add)
+
+    #add cavities
+    for i in range(len(emptygrid)):
+        if i not in indexes_to_pop:
+            coordlistpart2.append(emptylist[i])
+    npcoord2 = np.array((coordlistpart2))
+
+
+
+    #PART 2 ANSWER
+    number_of_cubes = len(npcoord2)
+    kd_tree = KDTree(npcoord2)
+
+    start_time = time.time()
+    count = kd_tree.count_neighbors(kd_tree, 1)
+    print("answer Part 2: ", number_of_cubes * 7 - count)
+    print("count neighbour took ", time.time() - start_time, " to run")
+
+    start_time = time.time()
+    indexes = kd_tree.query_ball_tree(kd_tree, r=1)
+    count2 = sum([len(i) for i in indexes])
+    print("answer Part 2: ", number_of_cubes * 7 - count2)
+    print("query ball took ", time.time() - start_time, " to run")
+
+    # #visualise point clouds
+    # pcd = o3d.geometry.PointCloud()
+    # pcd.points = o3d.utility.Vector3dVector(npcoord2)
+    # o3d.visualization.draw_geometries([pcd])
+
+    print("\n")
+
+
+
+
+
+
+
+
+
+
