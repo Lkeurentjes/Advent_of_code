0c5852343b677f62eab1414fa9d0dbf0a6459e33
Lisa Keurentjes <43202623+Lkeurentjes@users.noreply.github.com>
2022-12-20 17:02:35 +0100

    fixed day 8

--- 2022-08Treehouse/2022-08Treehouse.py
+++ 2022-08Treehouse/2022-08Treehouse.py
@@ -1,8 +1,5 @@
-with open('2022-08Treehouse.txt') as f:
-    lines = f.read().splitlines()
-    print(lines)
-
 import numpy as np
+
 
 class Grid:
     def __init__(self, lines):
@@ -16,7 +13,7 @@ class Grid:
                 row.append(int(lines[i][j]))
             self.grid.append(row)
 
-        self.printgrid()
+        # self.printgrid()
         self.vissablelr = np.zeros((self.height, self.width))
         self.vissablerl = np.zeros((self.height, self.width))
         self.vissableud = np.zeros((self.height, self.width))
@@ -25,10 +22,8 @@ class Grid:
 
         self.treehousevalues = np.zeros((self.height, self.width))
 
-
     def printgrid(self):
         for i in self.grid:
-            # pass
             print(i)
 
     def visability(self):
@@ -54,20 +49,17 @@ class Grid:
                 if self.grid[j][i] > seevalue:
                     self.vissableud[j][i] = 1
                     seevalue = self.grid[j][i]
-
                 if self.grid[j][i] == 9:
                     break
 
         # right
         for i in range(self.height):
-            seevalue = self.grid[i][self.height-1]
-            for j in range(self.width-1,0,-1):
-                if j == self.width-1:
+            seevalue = self.grid[i][self.height - 1]
+            for j in range(self.width - 1, 0, -1):
+                if j == self.width - 1:
                     self.vissablerl[i][j] = 1
-
                 if self.vissablelr[i][j] == 1:
                     break
-
                 if self.grid[i][j] > seevalue:
                     self.vissablerl[i][j] = 1
                     seevalue = self.grid[i][j]
@@ -76,69 +68,68 @@ class Grid:
 
         # down
         for i in range(self.width):
-            seevalue = self.grid[self.width-1][i]
-            for j in range(self.height-1,0,-1):
-                if j == self.height-1:
+            seevalue = self.grid[self.width - 1][i]
+            for j in range(self.height - 1, 0, -1):
+                if j == self.height - 1:
                     self.vissabledu[j][i] = 1
-
-                if self.vissableud[j][i] ==1:
+                if self.vissableud[j][i] == 1:
                     break
-
                 if self.grid[j][i] > seevalue:
                     self.vissabledu[j][i] = 1
                     seevalue = self.grid[j][i]
-
                 if self.grid[j][i] == 9:
                     break
 
-
-        # # print(self.vissablelr)
-        # # print(self.vissableud)
-        # # print(self.vissablerl)
-        # # print(self.vissabledu)
+        # print(self.vissablelr)
+        # print(self.vissableud)
+        # print(self.vissablerl)
+        # print(self.vissabledu)
         # print(self.vissablelr+self.vissableud+self.vissablerl+self.vissabledu)
-        # # print(np.count_nonzero(self.vissablelr + self.vissableud + self.vissablerl + self.vissabledu))
+        return (np.count_nonzero(self.vissablelr + self.vissableud + self.vissablerl + self.vissabledu))
 
     def treehouse(self):
-        for i in range(self.height):
-            for j in range(self.width):
-                tree = self.grid[i][j]
         best = 0
-        # for y, row in enumerate(self.grid):
-        #     for x, tree in enumerate(row):
-        #         # views = [self.grid[y][:x][::-1], self.grid[y][x + 1:], T[x][:y][::-1], T[x][y + 1:]]
-        #         score = 1
-        #         for view in views:
-        #             try:
-        #                 score *= list(v >= tree for v in view).index(True) + 1
-        #             except ValueError:
-        #                 score *= len(view)
-        #         best = max(score, best)
-        # print(best)
+        for i in range(self.height):
 
+            for j in range(self.width):
+                score = 1
+                viewvertical = [[*range(i-1,-1, -1)], [*range(i + 1, self.height)]]
+                for view in viewvertical:
+                    scorev = 0
+                    height = self.grid[i][j]
+                    for v in view:
+                        if self.grid[v][j] < height:
+                            scorev += 1
+
+                        else:
+                            scorev += 1
+                            break
+                    # print("score: ",scorev)
+                    score *= scorev
+
+                viewhorizontal = [[*range(j-1,-1,-1)], [*range(j + 1, self.width)]]
+                for view in viewhorizontal:
+                    scorev = 0
+                    height = self.grid[i][j]
+                    for v in view:
+
+                        if self.grid[i][v] < height:
+                            scorev += 1
+
+                        else:
+                            scorev += 1
+                            break
+                    # print("score: ",scorev)
+                    score *= scorev
+                # print(score)
+
+                best = max(score, best)
+        return best
 
 
 with open('2022-08Treehouse.txt') as f:
     lines = f.read().splitlines()
-    landscape = Grid(lines)
-    # landscape.visability()
-    landscape.treehouse()
 
-with open('2022-08Treehouse.txt') as f:
-    A = list(f.read().strip().splitlines())
-
-A = [[int(c) for c in line.strip()] for line in A]
-T = list(zip(*A))
-
-best = 0
-for y, row in enumerate(A):
-    for x, tree in enumerate(row):
-        views = [A[y][:x][::-1], A[y][x + 1:], T[x][:y][::-1], T[x][y + 1:]]
-        score = 1
-        for view in views:
-            try:
-                score *= list(v >= tree for v in view).index(True) + 1
-            except ValueError:
-                score *= len(view)
-        best = max(score, best)
-print(best)
+landscape = Grid(lines)
+print("PART 1: Trees vissable from the outside are", landscape.visability())
+print("PART 2: best location has a scenic view of", landscape.treehouse())
--- README.md
+++ README.md
@@ -2,7 +2,6 @@
  Advent of code solutions in python, using various algorithms.
 
 ## Todo's
-- 8, doesn't work
 - 14, optimize part 2
 - 15, optimize part 2
 - 17, doesn't work
