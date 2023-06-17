01be924a42f7652e7f3f7b48823658314ed156e3
Lisa Keurentjes <43202623+Lkeurentjes@users.noreply.github.com>
2023-06-17 19:51:45 +0200

    add day 5 part 2

--- 2021/2021-05Hydrothermal-Venture/2021-05Hydrothermal-Venture.py
+++ 2021/2021-05Hydrothermal-Venture/2021-05Hydrothermal-Venture.py
@@ -2,28 +2,28 @@ import numpy as np
 
 
 class Grid:
-    def __init__(self,lines):
+    def __init__(self, lines):
         self.valveslist = self.makelinesList(lines)
         self.diagonal = []
         self.max = self.valveslist.max(axis=0).max(axis=0)
-        self.grid = np.zeros([self.max[0]+1,self.max[1]+1])
+        self.grid = np.zeros([self.max[0] + 1, self.max[1] + 1])
         self.addvalves()
         # print(self.grid)
-        self.part1 =len(np.where(self.grid>=2)[0])
+        self.part1 = len(np.where(self.grid >= 2)[0])
         self.adddiagonals()
+        # print(np.rot90(np.flip(self.grid, axis =1)))
         self.part2 = len(np.where(self.grid >= 2)[0])
 
     def makelinesList(self, lines):
-        list =[]
+        list = []
         for line in lines:
             xy = []
             for l in line.split(" -> "):
-                l=l.split(",")
+                l = l.split(",")
                 l[0] = int(l[0])
                 l[1] = int(l[1])
                 xy.append(l)
             list.append(xy)
-        print(list)
         return np.array(list)
 
     def addvalves(self):
@@ -34,24 +34,29 @@ class Grid:
                     step = 1
                 else:
                     step = -1
-                for i in range(valve[0][1],valve[1][1]+step, step):
-                    self.grid[valve[0][0],i] += 1
+                for i in range(valve[0][1], valve[1][1] + step, step):
+                    self.grid[valve[0][0], i] += 1
 
             elif valve[0][1] == valve[1][1]:
                 if valve[0][0] < valve[1][0]:
                     step = 1
                 else:
                     step = -1
-                for i in range(valve[0][0], valve[1][0]+step, step):
+                for i in range(valve[0][0], valve[1][0] + step, step):
                     self.grid[i, valve[0][1]] += 1
 
             else:
                 self.diagonal.append(valve)
 
     def adddiagonals(self):
-        pass
-
-
+        for valve in self.diagonal:
+            stepx = (valve[1][0] - valve[0][0])
+            stepy = (valve[1][1] - valve[0][1])
+            i, j = 1, 1
+            if stepx < 0: i = -1
+            if stepy < 0: j = -1
+            for x, y in zip(range(0, stepx +i, i), range(0, stepy + j, j)):
+                self.grid[valve[0][0] + x, valve[0][1] + y] += 1
 
 
 with open('2021-05Hydrothermal-Venture.txt') as f:
@@ -59,4 +64,4 @@ with open('2021-05Hydrothermal-Venture.txt') as f:
 
 oceanfloor = Grid(lines)
 print("Part 1: there are", oceanfloor.part1, "Points with score 2 or more")
-print("Part 2: there are", oceanfloor.part1, "Points with score 2 or more")
+print("Part 2: there are", oceanfloor.part2, "Points with score 2 or more")
--- 2021/README.md
+++ 2021/README.md
@@ -6,10 +6,17 @@
 - day 1 - Sonar Sweep
   - For part 1 you need to find out how many times the depth increases, which is done by looping over the depth and when the next is bigger, it increases the counter
   - For part 2 you need to find out how many times the sum of three depth increases, which is done by looping over the depth with use of a range and when the sums next are bigger, it increases the counter
-- day 2 - Rock, Paper, Scissors
-  - For Part 1 you need to calculate the total score of the game according to a strategy, this is done by making a dictionary with points for each outcome and the loop over the outcomes and translate them into scores which are summed.
-  - For part 2 you need to do the same, but with a different score, so another dictionary is used
+- day 2 - Dive
+  - For part 1 you need to find the end position of the submarine after the planned course. This is done by sums and substractions of the course.
+  - For part 2 you need to find the end position of the submarine after the planned course, but depth is now decided on aim. This is done by sums and substractions and multiplications of the course.
+- day 3 - Binary Diagnostic
+  - For part 1 you need to find 2 rates based on most common and least common bits. This is done by looping over the bits with a count function.
+  - For Part 2 you need to find 2 rates based on most common and least common, but after choosing a number, the other binaries dissapear. This is done by a count function and removing the numbers not used anymore from the list.
+- day 5 - Hydrothermal Venture
+  - For Part 1 you need to find density of vents in a grid (considering only horizontal and vertical), this is done by making a np zeros matrix as grid and project the vents onto the grid by making the grid value +=1. The "too dense" points are the calculated with the np where function.
+  - For Part 2 you need to find density of vents in a grid (also considering the diagonal vents), this is done by making a np zeros matrix as grid and project the vents onto the grid by making the grid value +=1. The "too dense" points are the calculated with the np where function.
 
+  - 
 
 ## Todo's
 
