7f2436e7204abc76884c6cc1b3c0548bd474c337
Lisa Keurentjes <43202623+Lkeurentjes@users.noreply.github.com>
2024-11-14 17:05:42 +0100

    better day 23 of 2023

--- 2023/2023-23-Long-Walk/2023-23-Long-Walk.py
+++ 2023/2023-23-Long-Walk/2023-23-Long-Walk.py
@@ -1,6 +1,5 @@
-import numpy as np
 import sys
-from queue import PriorityQueue
+from collections import defaultdict
 
 sys.setrecursionlimit(10 ** 6)
 
@@ -9,161 +8,133 @@ class Grid:
     def __init__(self, lines):
         self.height = len(lines)
         self.width = len(lines[0])
-        self.grid = np.array([[c for c in row] for row in lines])
-        self.directions = {"^": (-1, 0), "v": (1, 0), "<": (0, -1), ">": (0, 1)}
-        self.printgrid()
+        self.grid = [[c for c in row] for row in lines]
+        self.directions = {"^": (-1, 0),
+                           "v": (1, 0),
+                           "<": (0, -1),
+                           ">": (0, 1),
+                           '.': [(0, 1), (0, -1), (-1, 0), (1, 0)]}
+        self.start = (0, 1)
         self.finish = (self.height - 1, self.width - 2)
-        self.visited = dict()
-        self.graph = self.make_graph()
 
+        # Part2 Graph
+        self.graph = defaultdict(list)
+        self.nodes = set()
+        self.edges = defaultdict(list)
+        self.build_graph()
+        print(self.graph)
 
     def printgrid(self):
         print("garden")
         for row in self.grid:
             print("".join("■" if cell == "#" else "." for cell in row))
 
-    def find_fork(self, start):
-        Queue = [start]
-        length = 0
-        been = []
-        while Queue:
-            x,y = Queue.pop(0)
-            if (x,y) in been:
+    def is_valid(self, x, y, seen):
+        return (0 <= x < self.width and
+                0 <= y < self.height and
+                self.grid[x][y] != '#' and
+                (x,y) not in seen)
+
+    def dfs(self,x,y,seen, length, slopes):
+        max_length = 0
+
+        if  (x,y) == self.finish:
+            return length
+
+        moves = []
+        if slopes and self.grid[x][y] in '>^v<':
+            moves.append(self.directions[self.grid[x][y]])
+        else:
+            moves.extend(self.directions['.'])
+
+        for dx, dy in moves:
+            nx, ny = x + dx, y + dy
+            if self.is_valid(nx, ny, seen):
+                seen.add((nx, ny))
+                max_length = max(max_length, self.dfs(nx, ny, seen, length+1, slopes))
+                seen.remove((nx, ny))
+
+        return max_length
+
+    def find_longest_path(self, slopes = True):
+        x, y = self.start
+        seen = set()
+        seen.add((x, y))
+        return self.dfs(x,y,seen,0, slopes)
+
+    def build_graph(self):
+        directions = [(0, -1), (1, 0), (0, 1), (-1, 0)]  # ^>v<
+        for y in range(self.width):
+            for x in range(self.height):
+                cell = self.grid[x][y]
+                if cell != '#':  # If the current cell is not a wall
+                    for dx, dy in directions:
+                        nx, ny = x + dx, y + dy
+                        if 0 <= nx < self.width and 0 <= ny < self.height:  # Check bounds
+                            neighbour = self.grid[nx][ny]
+                            if neighbour != '#':  # Add valid neighbors
+                                self.graph[(x, y)].append((nx, ny))
+
+        # Debugging for start and finish nodes
+        print(f"Start node connections: {self.graph[self.start]}")
+        print(f"Finish node connections: {self.graph[self.finish]}")
+        self.find_vertices_start_end()
+        self.find_edges()
+
+    def find_vertices_start_end(self):
+        for (x, y), neighbours in self.graph.items():
+            no_of_neighbours = len(neighbours)
+            if no_of_neighbours != 2:  # Junctions or endpoints
+                self.nodes.add((x, y))
+
+
+
+    def find_edges(self):
+        for node in self.nodes:
+            queue = [(node, set())]
+
+            while queue:
+                (x, y), visited = queue.pop()
+
+                for neighbour in self.graph[(x, y)]:
+                    if neighbour not in visited:
+                        if neighbour in self.nodes:
+                            self.edges[node].append((neighbour, len(visited) + 1))
+                        else:
+                            queue.append((neighbour, visited | {(x, y)}))
+
+    def find_longest_path_graph(self):
+        path_distances = []
+        queue = [(self.start, set(), 0)]
+
+        while queue:
+            (x, y), visited, distance = queue.pop()
+
+            if (x, y) == self.finish:
+                path_distances.append(distance)
                 continue
-            been.append((x,y))
-            to = []
-            for i, j in self.directions.values():
-                if 0 <= x + i < self.height and 0 <= y + j < self.width and self.grid[x+i][y+j] != "#" and (x +i,y+j) not in been:
-                    to.append((x+i, y+j))
-            if len(to) == 0:
-                return 1,2,3
-            if len(to) != 1:
-                return length, to
-            Queue.append(to[0])
-            length += 1
 
-    def make_graph(self):
-        a=1
+            for neighbour_node, weight in self.edges[(x, y)]:
+                if neighbour_node not in visited:
+                    queue.append((neighbour_node, visited | {(x, y)}, distance + weight))
 
-
-    def longest_path(self, current, visited=set(), dist=0, max_dist=0, slopes=True):
-        x, y = current
-        if self.grid[x][y] == "#" or current in visited:
+        if not path_distances:
+            print("No path found from start to finish.")
             return 0
 
-        if current == self.finish:
-            print(max_dist, dist)
-            return max(max_dist, dist)
-
-
-
-        visited.add(current)
-        # print(current, dist, max_dist, visited)
-        for i, j in self.directions.values():
-            if 0 <= x + i < self.height and 0 <= y + j < self.width:
-                new = (x + i, y + j)
-                if slopes and self.grid[x + i][y + j] in self.directions.keys():
-                    # print("heeere")
-                    k, l = self.directions[self.grid[x + i][y + j]]
-                    max_dist = max(max_dist,
-                                   self.longest_path((x + i + k, y + j + l), visited, dist + 2, max_dist, slopes))
-                else:
-                    max_dist = max(max_dist, self.longest_path((x + i, y + j), visited, dist + 1, max_dist, slopes))
-
-        visited.remove(current)
-
-        return max_dist
-
-    def longest_path_memo(self, current, visited=set(), dist=0, max_dist=0, slopes=True):
-        x, y = current
-        if self.grid[x][y] == "#" or current in visited:
-            return 0
-
-        if current == self.finish:
-            print(max_dist, dist)
-            return max(max_dist, dist)
-
-        check = self.visited.get(current, 0)
-        if check !=0:
-            for d, v in check:
-                if visited.issubset(v):
-                    return 0
-                # if d < dist:
-                #     return 0
-
-
-        visited.add(current)
-        # print(current, dist, max_dist, visited)
-        for i, j in self.directions.values():
-            if 0 <= x + i < self.height and 0 <= y + j < self.width:
-                # self.visited.setdefault(current, []).append(((0, visited)))
-                new = (x + i, y + j)
-                if slopes and self.grid[x + i][y + j] in self.directions.keys():
-                    # print("heeere")
-                    k, l = self.directions[self.grid[x + i][y + j]]
-                    max_dist = max(max_dist,
-                                   self.longest_path((x + i + k, y + j + l), visited, dist + 2, max_dist, slopes))
-                else:
-                    max_dist = max(max_dist, self.longest_path((x + i, y + j), visited, dist + 1, max_dist, slopes))
-
-        self.visited.setdefault(current, []).append(((max_dist, visited)))
-        visited.remove(current)
-
-
-
-
-        return max_dist
-
-    # def longest_path_memo(self, current, dist=0, max_dist=0, slopes=True):
-    #     Queue = PriorityQueue()
-    #     Queue.put((dist, current, {0}))
-    #     visited_far = dict()
-    #     visited_how = dict()
-    #     while not Queue.empty():
-    #         # print(visited_far)
-    #         dist, (x,y), visited = Queue.get()
-    #         dist *= -1
-    #         print(dist)
-    #         if self.grid[x][y] == "#" or (x,y) in visited:
-    #             continue
-    #
-    #         if dist > visited_far.get((x,y), [-1,0])[0] :
-    #             visited_far[(x,y)] = (dist, list(visited)[-1])
-    #         else:
-    #             if visited_far.get((x,y), [-1,0])[1] == list(visited)[-1]:
-    #                 continue
-    #
-    #         if (x,y) == self.finish:
-    #             continue
-    #
-    #         visited.add((x,y))
-    #         for i, j in self.directions.values():
-    #             if 0 <= x + i < self.height and 0 <= y + j < self.width:
-    #                 v_copy = visited.copy()
-    #                 if slopes and self.grid[x + i][y + j] in self.directions.keys():
-    #                     k, l = self.directions[self.grid[x + i][y + j]]
-    #                     Queue.put((-(dist + 2),(x + i + k, y + j + l), v_copy))
-    #                 else:
-    #                     Queue.put((-(dist + 1), (x + i, y + j), v_copy))
-    #
-    #
-    #     return visited_far[self.finish]
-
-
+        return max(path_distances)
 
 
 
 
 with open('2023-23-Long-Walk.txt') as f:
     lines = f.read().splitlines()
-    print(lines)
+
 Maze = Grid(lines)
-print("Part 1, Longest path is:", Maze.longest_path((0, 1)))
-input("Hit enter")
-# print("Part 1.1, Longest path is:", Maze.longest_path_memo((0, 1)))
-# input("Hit enter")
-print("Part 2.1, Longest path is:", Maze.longest_path_memo((0, 1), slopes=False))
-input("Hit enter")
-# print("Part 2, Longest path is:", Maze.longest_path((0, 1), slopes=False))
-# input("Hit enter")
+print("Part 1, Longest path is:", Maze.find_longest_path())
+
+result = Maze.find_longest_path_graph()
+print("Part 2, Longest path is:", result)
+
+print("Part 2, Longest path is:", Maze.find_longest_path(False))
 
